98239d984c173a037de742a2a3c1edca05bbcf0c
iliyaztalipov <italipov1910@gmail.com>
2019-01-27 19:57:20 +0600

    add task model

---
+++ app/src/main/java/com/example/window/Models/Task.java
@@ -0,0 +1,12 @@
+package com.example.window.Models;
+
+import java.util.Date;
+
+public class Task {
+
+    String title;
+    String description;
+    Boolean isDone;
+    Date creationDate;
+    Date endDate;
+}
